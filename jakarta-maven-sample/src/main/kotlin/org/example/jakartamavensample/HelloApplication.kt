1a6d97dabed48a92803482754236bc360364edc7
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2025-05-20 11:10:42 +0200

    Remove other samples

--- jakarta-maven-sample/src/main/kotlin/org/example/jakartamavensample/HelloApplication.kt
+++
@@ -1,9 +0,0 @@
-package org.example.jakartamavensample
-
-import jakarta.ws.rs.ApplicationPath
-import jakarta.ws.rs.core.Application
-
-@ApplicationPath("/api")
-class HelloApplication : Application() {
-
-}
